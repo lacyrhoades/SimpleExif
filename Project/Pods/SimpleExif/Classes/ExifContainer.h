4d838f0b4979bd8bacc009bbbecf4242efda164e
Lacy Rhoades <lacyrhoades@gmail.com>
2016-12-07 14:08:22 -0500

    Oh yeahhhhh header files

--- Project/Pods/SimpleExif/Classes/ExifContainer.h
+++ Project/Pods/SimpleExif/Classes/ExifContainer.h
@@ -17,6 +17,8 @@
 - (void)addCreationDate:(NSDate *)date;
 - (void)addDescription:(NSString *)description;
 - (void)addProjection:(NSString *)projection;
+- (void)addLensModel:(NSString *)model;
+- (void)addLensMake:(NSString *)make;
 
 - (void)setValue:(NSString *)key forExifKey:(NSString *)value;
 
